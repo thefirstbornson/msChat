da1d9e20702c68d1418aae451e154d18d001a9ea
Artem Poskrebyshev <a.poskrebyshev@decosystems.ru>
2020-10-29 12:55:19 +0200

    Deleted SwaggerConfig.java, added README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Getting Started
+
+### Сборка 
+Чтобы собрать проект:
+* gradle build bootRun
+
+### Swagger
+The following guides illustrate how to use some features concretely:
+* http://localhost:8080/swagger-ui/index.html
+
+или
+* http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config
--- adapter/controller/src/main/java/vtb/app/adapter/controller/TokenController.java
+++ adapter/controller/src/main/java/vtb/app/adapter/controller/TokenController.java
@@ -2,7 +2,6 @@ package vtb.app.adapter.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.codec.binary.Base64;
@@ -10,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import vtb.app.service.TokenService;
 
 import java.io.IOException;
 
@@ -17,10 +17,10 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class TokenController {
     public static final String CONTEXT_USER_UUID_JSON_FIELD = "ctxi";
-//    private final TokenService tokenService;
+    private final TokenService tokenService;
     private final ObjectMapper objectMapper;
 
-    @PostMapping(value ="/chat/sendtoken")
+    @PostMapping(value ="/api/user/sendchattoken")
     public ResponseEntity<?> sendToken( @Parameter(hidden = true) @RequestHeader(name="Authorization") String token){
         String sessionId = getSessionId(token);
         return ResponseEntity.ok(sessionId);
@@ -31,8 +31,8 @@ public class TokenController {
         String payload = token.substring(token.indexOf(".")+1, token.lastIndexOf("."));
         String decodedPayload = new String(new Base64().decode(payload.getBytes()));
         try {
-            ObjectNode node = objectMapper.readValue(decodedPayload, ObjectNode.class);
-            sessionId = node.get(CONTEXT_USER_UUID_JSON_FIELD).toString();
+            ObjectNode json = objectMapper.readValue(decodedPayload, ObjectNode.class);
+            sessionId = json.get(CONTEXT_USER_UUID_JSON_FIELD).toString();
         } catch (NullPointerException | IOException exception){
             throw new IllegalArgumentException("Invalid token");
         }
